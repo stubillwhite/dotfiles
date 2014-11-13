b7571cb6ef59ba7bd41c4349705cb7afa847050c
Stu White <stubillwhite@gmail.com>
2014-11-13 13:34:58 +0000

    Updated Leiningen configuration, added Growl configuration.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user 
  {:plugins [ [lein-midje "3.1.3"]
-             [cider/cider-nrepl "0.7.0"] 
+             [cider/cider-nrepl "0.8.0-SNAPSHOT"] 
+             [lein-ancient "0.5.5"]
            ]
  }
 }
---
+++ growl/readme.md
@@ -0,0 +1,3 @@
+This configuration file needs to be put in:
+
+`C:\Users\IBM_ADMIN\Local Settings\Application Data\Growl\2.0.0.0`
