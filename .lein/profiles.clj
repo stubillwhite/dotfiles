f1bd22958b32c1c42dbf7fd7b13628786076e907
Stu White <stubillwhite@gmail.com>
2015-06-24 14:02:16 +0100

    Included Clojure code quality plugins.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,14 @@
-{:user 
- {:plugins [ [lein-midje "3.1.3"]
-             [cider/cider-nrepl "0.9.0"]
-             [lein-ancient "0.5.5"]
-           ]
- }
+{
+  :user 
+  {
+    :plugins
+    [ [cider/cider-nrepl "0.9.0"]
+      [jonase/eastwood "0.2.1"]
+      [lein-ancient "0.5.5"]
+      [lein-kibit "0.1.2"]
+      [lein-midje "3.1.3"] ]
+    
+    :aliases
+    { "jfdi"
+      ["do" ["clean"] ["ancient"] ["kibit"] ] } }
 }
