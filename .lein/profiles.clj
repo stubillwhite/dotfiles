8f03ccc19c575b62b5866b46dea5fee694f7f696
stubillwhite <stubillwhite@gmail.com>
2015-07-23 22:30:16 +0100

    Updated dependencies.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,9 +2,9 @@
   :user 
   {
     :plugins
-    [ [cider/cider-nrepl "0.9.0"]
+    [ [cider/cider-nrepl "0.9.1"]
       [jonase/eastwood "0.2.1"]
-      [lein-ancient "0.5.5"]
+      [lein-ancient "0.6.7"]
       [lein-kibit "0.1.2"]
       [lein-midje "3.1.3"] ]
     
