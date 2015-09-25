2141e4235fb3041f5dab3893e8a202466e39b3df
Stu White <stubillwhite@gmail.com>
2015-09-25 15:25:09 +0100

    Updated Leiningen profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,7 +6,10 @@
       [jonase/eastwood "0.2.1"]
       [lein-ancient "0.6.7"]
       [lein-kibit "0.1.2"]
-      [lein-midje "3.1.3"] ]
+      [lein-midje "3.1.3"]
+      [slamhound "1.5.5"]
+      [refactor-nrepl "1.1.0"]
+      ]
     
     :aliases
     { "jfdi"
