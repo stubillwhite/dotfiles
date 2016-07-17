d44d4bc1f0905856aa12edaa2b89e8e72b1d3893
stubillwhite <stubillwhite@gmail.com>
2016-07-17 10:45:13 +0100

    Update Clojure profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,15 +1,14 @@
-{:user {:plugins     [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                      [jonase/eastwood "0.2.2"]
-                      [lein-ancient "0.6.8"]
-                      [lein-kibit "0.1.2"]
-                      [lein-midje "3.2"]
-                      [lein-expectations "0.0.8"]
-                      [lein-autoexpect "1.7.0"] 
-                      [slamhound "1.5.5"]
-                      [criterium "0.4.3"]
-                      [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        
+{
+ :user {:plugins      [[jonase/eastwood "0.2.3"]
+                       [lein-ancient "0.6.10"]
+                       [lein-kibit "0.1.2"]
+                       [slamhound "1.5.5"]
+                       [criterium "0.4.4"]]
+
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-                       
-        :aliases      { "sanity-check" ["do" ["clean"] ["ancient"] ["kibit"]]}}
- }
+        
+        :aliases      {"sanity-check" ["do" ["clean"] ["ancient"] ["kibit"]]}}
+
+ :repl {:plugins      [[cider/cider-nrepl "0.12.0"]
+                       [refactor-nrepl "2.2.0"]]}
+}
