a2febdb56b11de074ea6cf9d30c06a8e15f4e1a7
Stu W <stubillwhite@gmail.com>
2018-07-06 18:01:26 +0100

    Update Leiningen config

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,11 +1,11 @@
-{:user {:plugins      [[jonase/eastwood "0.2.4"]
-                       [lein-ancient "0.6.14"]
-                       [lein-kibit "0.1.5"]
+{:user {:plugins      [[jonase/eastwood "0.2.7"]
+                       [lein-ancient "0.6.15"]
+                       [lein-kibit "0.1.6"]
                        [slamhound "1.5.5"]
                        [criterium "0.4.4"]
-                       [com.jakemccrary/lein-test-refresh "0.20.0"]]
+                       [com.jakemccrary/lein-test-refresh "0.22.0"]]
 
-        :dependencies [[pjstadig/humane-test-output "0.8.2"]
+        :dependencies [[pjstadig/humane-test-output "0.8.3"]
                        [org.clojure/tools.nrepl "0.2.13"]]
 
         :injections   [(require 'pjstadig.humane-test-output)
@@ -17,6 +17,6 @@
 
         :aliases      {"sanity-check" ["do" ["clean"] ["ancient"] ["kibit"]]}}
 
- :repl {:plugins      [[cider/cider-nrepl "0.14.0"]
+ :repl {:plugins      [[cider/cider-nrepl "0.17.0"]
                        [refactor-nrepl "2.3.0"]]}}
 
