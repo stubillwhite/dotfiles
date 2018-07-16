35f29ed68ef8d1009f4fa276a1124d6dc9ce850b
Stu W <stubillwhite@gmail.com>
2018-07-16 22:38:07 +0100

    Update Leiningen profile to match new Emacs

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins      [[jonase/eastwood "0.2.7"]
+{:user {:plugins      [[jonase/eastwood "0.2.8"]
                        [lein-ancient "0.6.15"]
                        [lein-kibit "0.1.6"]
                        [slamhound "1.5.5"]
                        [criterium "0.4.4"]
-                       [com.jakemccrary/lein-test-refresh "0.22.0"]]
+                       [com.jakemccrary/lein-test-refresh "0.23.0"]]
 
         :dependencies [[pjstadig/humane-test-output "0.8.3"]
                        [org.clojure/tools.nrepl "0.2.13"]]
@@ -17,6 +17,7 @@
 
         :aliases      {"sanity-check" ["do" ["clean"] ["ancient"] ["kibit"]]}}
 
- :repl {:plugins      [[cider/cider-nrepl "0.17.0"]
-                       [refactor-nrepl "2.3.0"]]}}
+ :repl {:plugins      [[cider/cider-nrepl "0.18.0snapshot"]
+                       ;; [refactor-nrepl "2.3.0"] -- v2.3.0 seems to be incompatible with cider-nrepl 0.17.0
+                       ]}}
 
