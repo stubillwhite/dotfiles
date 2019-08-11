0c12125f8f7f1ef6e946ce1c4d9e16d1fec7ea49
Stu W <stubillwhite@gmail.com>
2019-08-11 20:38:46 +0100

    Clean up aliases for leiningen

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -2,7 +2,6 @@
                        [lein-ancient "0.6.15"]
                        [lein-kibit "0.1.6"]
                        [lein-cloverage "1.1.1"]
-                       [slamhound "1.5.5"]
                        [criterium "0.4.4"]
                        [com.jakemccrary/lein-test-refresh "0.23.0"]]
 
@@ -14,11 +13,13 @@
         :injections   [(require 'pjstadig.humane-test-output)
                        (pjstadig.humane-test-output/activate!)]
 
-        :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
+        :test-refresh {:notify-command ["lein-autotest-notify"]
                        :quiet true
                        :changes-only true}
 
-        :aliases      {"sanity-check" ["do" ["clean"] ["ancient"] ["kibit"]]}}
+        :aliases      {"check"        ["do" ["clean"] ["ancient"] ["kibit"]]
+                       "coverage"     ["do" ["cloverage"]]
+                       "autotest"     ["do" ["test-refresh"]]}}
 
  :repl {:plugins      [[cider/cider-nrepl "0.21.1"]
                        ;; [refactor-nrepl "2.3.0"] -- v2.3.0 seems to be incompatible with cider-nrepl 0.17.0
