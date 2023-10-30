56c4e2f47213717b93e4ec5542c160f545301f3d
Stu W <stubillwhite@gmail.com>
2023-10-30 15:32:27 +0000

    Add OSX configuration for hot corners

--- configure-osx.sh
+++ configure-osx.sh
@@ -3,13 +3,13 @@
 function configure-osx-preferences() {
     echo 'Configuring OSX preferences'
 
-    echo "Keyboard repeat should start instantly"
+    echo "- Keyboard repeat should start instantly"
     defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-    echo "Keyboard repeat should be as fast as possible"
+    echo "- Keyboard repeat should be as fast as possible"
     defaults write NSGlobalDomain KeyRepeat -int 1
 
-    echo "AppSwitcher should display on all monitors"
+    echo "- AppSwitcher should display on all monitors"
     defaults write com.apple.dock appswitcher-all-displays -bool true
 
     # Hot corners
@@ -25,16 +25,17 @@ function configure-osx-preferences() {
     # 11: Launchpad
     # 12: Notification Center
     # 13: Lock Screen
-    echo "No hot corners"
+    echo "- Hot corners should allow Cmd+BottomRight to lock screen"
     defaults write com.apple.dock wvous-tl-corner   -int 0
     defaults write com.apple.dock wvous-tl-modifier -int 0
     defaults write com.apple.dock wvous-tr-corner   -int 0
     defaults write com.apple.dock wvous-tr-modifier -int 0
     defaults write com.apple.dock wvous-bl-corner   -int 0
     defaults write com.apple.dock wvous-bl-modifier -int 0
-    defaults write com.apple.dock wvous-br-corner   -int 0
-    defaults write com.apple.dock wvous-br-modifier -int 0
+    defaults write com.apple.dock wvous-br-corner   -int 13 
+    defaults write com.apple.dock wvous-br-modifier -int 1048576
 
+    echo
     echo "Killing affected applications"
 
     APPS=(
@@ -44,9 +45,11 @@ function configure-osx-preferences() {
     )
 
     for app in "${APPS[@]}"; do
+        echo "- ${app}"
         killall "$app" >/dev/null 2>&1
     done
 
+    echo
     echo "Note that some settings require a restart to take effect"
 }
 
