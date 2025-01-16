79bddd550dc68f7bebc78964b0f460181c69935a
Stu W <stubillwhite@gmail.com>
2025-01-16 15:25:32 +0000

    Update OSX config

--- configure-osx.sh
+++ configure-osx.sh
@@ -7,22 +7,46 @@
 # defaults domains      # List domains
 # defaults find WORD    # Search for WORD
 
+function msg-success() {
+    local msg=$1
+    printf "${CLEAR_LINE}${COLOR_GREEN}✔ ${msg}${COLOR_NONE}\n"
+}
+
+function msg-error() {
+    local msg=$1
+    printf "${CLEAR_LINE}${COLOR_RED}✘ ${msg}${COLOR_NONE}\n"
+}
+
+function configure-setting-if-present() {
+    local domain=$1
+    local key=$2
+    shift
+    shift
+    local values=$*
+
+    if defaults read "${domain}" "${key}" > /dev/null; then
+        defaults write "${domain}" "${key}" ${values}
+    else
+        msg-error "Setting ${domain} ${key} does not exist"
+    fi
+}
+
 function configure-osx-preferences() {
     echo 'Configuring OSX preferences'
 
     echo "- Keyboard repeat should start instantly"
-    defaults write NSGlobalDomain InitialKeyRepeat -int 15
+    configure-setting-if-present NSGlobalDomain InitialKeyRepeat -int 15
 
     echo "- Keyboard repeat should be as fast as possible"
-    defaults write NSGlobalDomain KeyRepeat -int 1
+    configure-setting-if-present NSGlobalDomain KeyRepeat -int 1
 
     echo "- AppSwitcher should display on all monitors"
-    defaults write com.apple.dock appswitcher-all-displays -bool true
+    configure-setting-if-present com.apple.dock appswitcher-all-displays -bool true
 
     echo "- Operating system should be silent"
-    defaults write com.apple.sound.beep.flash      -int 0
-    defaults write com.apple.sound.beep.volume     -int 0
-    defaults write com.apple.sound.uiaudio.enabled -int 0
+    configure-setting-if-present com.apple.sound.beep.flash      -int 0
+    configure-setting-if-present com.apple.sound.beep.volume     -int 0
+    configure-setting-if-present com.apple.sound.uiaudio.enabled -int 0
 
     # Hot corners
     # Possible values:
@@ -38,14 +62,18 @@ function configure-osx-preferences() {
     # 12: Notification Center
     # 13: Lock Screen
     echo "- Hot corners should allow Cmd+BottomRight to lock screen"
-    defaults write com.apple.dock wvous-tl-corner   -int 0
-    defaults write com.apple.dock wvous-tl-modifier -int 0
-    defaults write com.apple.dock wvous-tr-corner   -int 0
-    defaults write com.apple.dock wvous-tr-modifier -int 0
-    defaults write com.apple.dock wvous-bl-corner   -int 0
-    defaults write com.apple.dock wvous-bl-modifier -int 0
-    defaults write com.apple.dock wvous-br-corner   -int 13 
-    defaults write com.apple.dock wvous-br-modifier -int 1048576
+    configure-setting-if-present com.apple.dock wvous-tl-corner   -int 0
+    configure-setting-if-present com.apple.dock wvous-tl-modifier -int 0
+    configure-setting-if-present com.apple.dock wvous-tr-corner   -int 0
+    configure-setting-if-present com.apple.dock wvous-tr-modifier -int 0
+    configure-setting-if-present com.apple.dock wvous-bl-corner   -int 0
+    configure-setting-if-present com.apple.dock wvous-bl-modifier -int 0
+    configure-setting-if-present com.apple.dock wvous-br-corner   -int 13 
+    configure-setting-if-present com.apple.dock wvous-br-modifier -int 1048576
+
+    # Add pathbar to title
+    echo "- Finder should display path in title"
+    configure-setting-if-present com.apple.finder _FXShowPosixPathInTitle -bool true
 
     echo
     echo "Killing affected applications"
